ff8155e2e354a1a8d5b0fe9382d07bf66f781751
Greg Gascoigne <gas19002@byui.edu>
2026-01-08 11:40:13 -0700

    Commiting Prep1 code - 1/8/26

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your grade percentage in the class? ");
+       string grade = Console.ReadLine();
+       int percentage = int.Parse(grade);
+    
+        if (percentage >= 90)
+        {
+            Console.WriteLine("Your grade was an A. You passed!");
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine("Your grade was a B. You passed!");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine("Your grade was a C. You passed!");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("Your grade was a D. You did not pass, you have valuale experience for next time!");
+        }
+        else
+        {
+            Console.WriteLine("Your grade was a F. You did not pass, you have valuale experience for next time!");
+        }
     }
 }
